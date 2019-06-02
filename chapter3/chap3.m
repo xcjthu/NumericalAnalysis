aea33e6e79bdf157f4391a287c5aba5a0e7b3ea0
xcjthu <xiaocj16@mails.tsinghua.edu.cn>
2019-06-02 10:41:41 +0800

    complete code for chap3

---
+++ chapter3/chap3.m
@@ -0,0 +1,10 @@
+
+% (1)
+cholesky(10, ones(10, 1))
+
+%(2)
+cholesky(10, ones(10, 1) + 1e-7)
+
+%(3)
+cholesky(8, ones(8, 1))
+cholesky(12, ones(12, 1))
---
+++ chapter3/cholesky.m
@@ -0,0 +1,49 @@
+function ans = cholesky(n, x)
+
+H = hilb(n);
+
+% x = ones(n, 1);
+
+b = H * x;
+
+A = H;
+
+
+for j = 1:n
+	for k = 1:j-1
+		A(j, j) = A(j, j) - A(j, k)^2;
+	end
+	A(j, j) = sqrt(A(j, j));
+
+	for i = j+1:n
+		for k = 1:j-1
+			A(i, j) = A(i, j) - A(i, k) * A(j, k);
+		end
+		A(i, j) = A(i, j)/A(j, j);
+	end
+end
+L = A;
+
+for i = 1:n
+	for j = i+1:n
+		L(i,j) = 0;
+	end
+end
+
+y = L\b;
+x_ = L.'\y; 
+
+% 计算残差
+r = b - H * x_;
+
+% 计算误差
+deltx = x_ - x;
+
+rnorm = norm(r, inf);
+xnorm = norm(deltx, inf);
+
+fprintf('the value of residual is %f\n\n', rnorm);
+fprintf('the value of error is %f\n\n', xnorm);
+
+
+ans = [rnorm, xnorm]
