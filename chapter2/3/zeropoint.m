6305b04d158bc901ecb55b6503231a01855bdc1f
xcjthu <xiaocj16@mails.tsinghua.edu.cn>
2019-06-09 09:55:38 +0800

    final perfect

--- chapter2/3/zeropoint.m
+++ chapter2/3/zeropoint.m
@@ -2,9 +2,11 @@ xs = -50:0.1:50;
 plot(xs, bessel(xs));
 range = [0, 3.8, 7.1, 10.4, 13.5, 16.8, 20, 23, 26.2, 29.3, 32];
 ans = zeros(10, 1);
+hold on;
 for i = 1:10
     x = range(i:i+1);
     ans(i) = fzerotx(@bessel, x);
+    scatter(ans(i), 0);
 end
 disp(ans);
 
--- chapter3/chap3.m
+++ chapter3/chap3.m
@@ -1,4 +1,4 @@
-
+fprintf('n=%d\n', 10);
 % (1)
 cholesky(10, ones(10, 1))
 
@@ -6,5 +6,10 @@ cholesky(10, ones(10, 1))
 cholesky(10, ones(10, 1) + 1e-7)
 
 %(3)
+fprintf('n=%d\n', 8);
 cholesky(8, ones(8, 1))
+cholesky(8, ones(8, 1) + 1e-7)
+
+fprintf('n=%d\n', 12);
 cholesky(12, ones(12, 1))
+cholesky(12, ones(12, 1) + 1e-7)
